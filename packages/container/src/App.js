c5112419b7b5d1d576c3765623eb39bd0eefe2f9
Rui Lemos <rui.lemos.code@gmail.com>
2026-02-04 10:56:10 +0000

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <div>
+      <h1>
+        Container App
+        <hr />
+      </h1>
+      <MarketingApp />
+    </div>
+  );
+};
