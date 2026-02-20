ab0541013f54e6c1f89bfe526ab0108bdc47765c
Rui Lemos <rui.lemos.code@gmail.com>
2026-02-20 11:03:12 +0000

    Add Header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
   return (
-    <div>
-      <h1>
-        Container App !!!
-        <hr />
-      </h1>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
